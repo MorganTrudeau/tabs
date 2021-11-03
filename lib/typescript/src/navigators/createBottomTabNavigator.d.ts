637463270272b2b0df0da4e237799f007e1b6370
Morgan Trudeau <morgantrudeau@gmail.com>
2021-11-02 20:54:23 -0700

    Adding lib files

---
+++ lib/typescript/src/navigators/createBottomTabNavigator.d.ts
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import { AccessibilityRole, AccessibilityState } from 'react-native';
+import { NavigationRoute } from 'react-navigation';
+import { NavigationViewProps } from '../utils/createTabNavigator';
+import { NavigationTabProp, NavigationBottomTabOptions, BottomTabBarOptions, SceneDescriptorMap } from '../types';
+declare type Props = NavigationViewProps & {
+    getAccessibilityRole: (props: {
+        route: NavigationRoute;
+    }) => AccessibilityRole | undefined;
+    getAccessibilityStates: (props: {
+        route: NavigationRoute;
+        focused: boolean;
+    }) => AccessibilityState[];
+    lazy?: boolean;
+    tabBarComponent?: React.ComponentType<any>;
+    tabBarOptions?: BottomTabBarOptions;
+    navigation: NavigationTabProp;
+    descriptors: SceneDescriptorMap;
+    screenProps?: unknown;
+    position?: 'top' | 'bottom';
+};
+declare const _default: (routes: import("react-navigation").NavigationRouteConfigMap<NavigationBottomTabOptions, NavigationTabProp<NavigationRoute<import("react-navigation").NavigationParams>, any>>, config?: import("react-navigation").CreateNavigatorConfig<{}, import("react-navigation").NavigationTabRouterConfig, Partial<NavigationBottomTabOptions>, NavigationTabProp<NavigationRoute<import("react-navigation").NavigationParams>, any>> | undefined) => React.ComponentType<Pick<Props, "navigation" | "descriptors" | "screenProps" | "position" | "getAccessibilityRole" | "getAccessibilityStates" | "lazy" | "tabBarComponent" | "tabBarOptions"> & {
+    navigationConfig: any;
+}>;
+export default _default;
